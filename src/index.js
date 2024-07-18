28017523dfdf8752d957aca3dedac9bb3833b344
gabriela klewenhagen <gabriela.klewenhagen@gmail.com>
2024-07-18 22:13:58 +0200

    updated reference in "All cities" link

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ function updateCity(event) {
           "h:mm:ss [<small>]A[</small>]"
         )}</div>
         </div>
-        <a href="/">Home</a>`;
+        <a href="index.html">All cities</a>`;
 }
 
 updateTime();
